1d441e50ba99f64f0df5363dae5a69dbf212fa3c
Rckdrigo <rckdigomed@gmail.com>
2016-03-09 10:06:32 -0600

    First commit

---
+++ Assets/PlayMovie.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour
+{
+    void Start()
+    {
+        //Handheld.PlayFullScreenMovie("Secuencia01b.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+        Handheld.PlayFullScreenMovie("https://www.youtube.com/watch?v=nhPaWIeULKk", Color.black, FullScreenMovieControlMode.CancelOnInput);
+    }
+
+}
