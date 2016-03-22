381427945136062b7a16bc4f84dd4fae0025fc97
Rckdrigo <rckdigomed@gmail.com>
2016-03-22 12:56:04 -0600

    Rotation values

--- Assets/BottleShake.cs
+++ Assets/BottleShake.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class BottleShake : MonoBehaviour
 {
-    [Range(-15f, 15f)]
-    public float maxAngle, minAngle;
 
     void Start()
     {
-        GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, maxAngle);
+        GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, 0);
         StartCoroutine("Shake");
     }
 
@@ -19,44 +17,51 @@ public class BottleShake : MonoBehaviour
 
     IEnumerator Shake()
     {
-        for (int i = 0; i < 85; i++)
+        for (int i = 0; i < 42; i++)
         {
-            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, 0f), (float)i / 85f);
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, 6f), (float)i / 42f);
+            yield return null;
+        }
+
+        for (int i = 0; i < 43; i++)
+        {
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, 0f), (float)i / 43f);
             yield return null;
         }
 
         for (int i = 0; i < 12; i++)
         {
-            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, minAngle), (float)i / 12f);
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, -11.64f), (float)i / 12f);
             yield return null;
         }
 
         for (int i = 0; i < 9; i++)
         {
-            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, maxAngle), (float)i / 9f);
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, 8.97f), (float)i / 9f);
             yield return null;
         }
 
         for (int i = 0; i < 10; i++)
         {
-            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, minAngle), (float)i / 10f);
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, -10.69f), (float)i / 10f);
             yield return null;
         }
 
         for (int i = 0; i < 9; i++)
         {
-            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, maxAngle), (float)i / 9f);
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, 12.64f), (float)i / 9f);
             yield return null;
         }
 
         for (int i = 0; i < 10; i++)
         {
-            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, minAngle), (float)i / 10f);
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, -4.46f), (float)i / 10f);
             yield return null;
         }
 
         for (int i = 0; i < 170; i++)
         {
+            GetComponent<RectTransform>().rotation = Quaternion.Lerp(GetComponent<RectTransform>().rotation, Quaternion.Euler(0f, 0f, 0), (float)i / 70f);
             yield return null;
         }
 
